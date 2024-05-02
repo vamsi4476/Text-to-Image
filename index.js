bc2e08f5a04196d5a62dbd3f42c163271359c77a
vamsi4476 <gundavamsi01@gmail.com>
2024-05-02 16:32:06 -0400

    Error-with-get-<a>

--- index.js
+++ index.js
@@ -23,37 +23,22 @@ const client = new MonsterApiClient('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2
 app.get("/", (req, res) => {
    //input_txt = req.query.input_prompt;
 
-   res.render("index");
-
-   // Commented the below as we don't need it for now......
-
-  //  const model = 'txt2img'; // Replace with the desired model name
-  //  const input = {
-  //    prompt: 'stadium',
-  //    negprompt: 'deformed, bad anatomy, disfigured, poorly drawn face',
-  //    samples: 2,
-  //    steps: 50,
-  //    aspect_ratio: 'square',
-  //    guidance_scale: 7.5,
-  //    seed: 2414,
-  //    style: 'photographic'
-  //  };
- 
-  //  client.generate(model, input)
-  //    .then((response) => {
-  //      const url = response.output[0];
-  //      console.log(url);
-  //      console.log('Generated content:', response);
-  //      res.render("home",{url});
-       
-  //    })
-  //    .catch((error) => {
-  //      console.error('Error generating content:', error);
-  //      res.status(500).send("Error generating content");
-  //    });
- 
+   res.render("home");
 
 });
+
+app.get("/index",(req,res)=>{ // This page is for taking input fo image generation
+
+    res.render("index");
+})
+
+app.get("/chatCompletion",(req,res)=>{
+
+  res.render("chatCompletion");
+
+})
+
+/////////////////////////// Monster-API  ////////////////////////////////////
 
 app.get("/pictures_page/:param",(req,res)=>{
 
@@ -82,12 +67,59 @@ app.get("/pictures_page/:param",(req,res)=>{
       console.error('Error generating content:', error);
       res.status(500).send("Error generating content");
     });
-
-  
-  
 })
 
 
+//////////////////////////// Gemini API  /////////////////////////////////
 
 
 
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI("AIzaSyD6FOVvT3VX9035-T9wcrDIR6Q4rS24WXw");
+
+// ...
+
+const generationConfig = {
+  stopSequences: ["red"],
+  maxOutputTokens: 50,
+  temperature: 0.9,
+  topP: 0.1,
+  topK: 16,
+};
+
+const model = genAI.getGenerativeModel({ model: "gemini-pro"},  generationConfig );
+
+// For text-only input, use the gemini-pro model
+//const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+
+// async function run() {
+//   const prompt = "SUNY NEW PALTZ"
+
+//   const result = await model.generateContent(prompt);
+//   const response = await result.response;
+//   const text = response.text();
+//   console.log(text);
+//   chatting(text);
+// }
+// run();
+
+// function chatting(text){
+// app.get("/getchatResponse",(req,res)=>{
+//   console.log("hello .........");
+//   res.render("chatCompletion",{text:"hi"});
+
+// })
+
+// }
+
+app.get("/getchatResponse",(req,res)=>{
+  console.log("hello .........");
+  //res.render("chatCompletion",{text:"hi"});
+  res.render("chatCompletion");
+
+})
+
+//Is get request different from <a> request
